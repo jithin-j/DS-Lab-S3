3fde49d407b25889efb283ed39a9a24b4b813999
Jithin Jagadeesh <75089891+jithin-j@users.noreply.github.com>
2022-02-11 06:33:37 +0000

    Add files via upload

---
+++ Feb 11/qn23.c
@@ -0,0 +1,8 @@
+/* Create a binary search tree with the following operations: 
+1.Insert a new node . 
+2.Inorder traversal. 
+3.Preorder traversal. 
+4.Postorder traversal. 
+5.Delete a node.
+6. Search for an element */
+
