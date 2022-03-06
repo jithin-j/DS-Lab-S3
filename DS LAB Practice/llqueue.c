b5c0826e30d1667732733cbe178958bb04d2b77b
jithin-j <jithinjagadeesh1@gmail.com>
2022-03-06 19:28:38 +0530

    llQueue

--- DS LAB Practice/llqueue.c
+++ DS LAB Practice/llqueue.c
@@ -10,6 +10,8 @@ void enqueue(int x)
 {
     struct node *newnode;
     newnode = (struct node *)malloc(sizeof(struct node));
+    newnode->data = x;
+    newnode->next = NULL;
     if (front == NULL && rear == NULL)
     {
         front = rear = newnode;
@@ -19,4 +21,42 @@ void enqueue(int x)
         rear->next = newnode;
         rear = newnode;
     }
+}
+void display()
+{
+    struct node *temp;
+    temp = front;
+    if (front == NULL && rear == NULL)
+    {
+        printf("Queue empty! \n");
+    }
+    else
+    {
+        while (temp != NULL)
+        {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+}
+void dequeue()
+{
+    if (front == NULL && rear == NULL)
+    {
+        printf("Queue empty! \n");
+    }
+    else
+    {
+        front = front->next;
+    }
+}
+void main()
+{
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    display();
+    dequeue();
+    display();
 }
